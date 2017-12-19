bd2376ff901c18731cfbe1b963888aa4e9d54167
bhoopendra <bhoopendra.akgec@gmail.com>
2017-12-19 08:48:38 +0530

    Showing moves

--- src/index.js
+++ src/index.js
@@ -89,12 +89,13 @@ class Game extends React.Component {
 			history:[{
 				squares : Array(9).fill(null),
 			}],
+			stepNumber : 0,
 			xIsNext : true,
 		};
 	}
 	
 	 handleClick(i){
-	  const history = this.state.history;
+	  const history = this.state.history.slice(0,this.state.stepNumber+1);
 	  const current = history[history.length-1];
 	  
 	  const squares = current.squares.slice();
@@ -109,11 +110,28 @@ class Game extends React.Component {
 	  });
   }
   
-	
+  jumpTo(step){
+	  this.setState({
+		  stepNumber:step,
+		  xIsNext: (step % 2) === 0,
+	  });
+  }
+  
   render() {
 	   const history = this.state.history;
-	  const current = history[history.length-1];
+	  const current = history[this.state.stepNumber];
 	  const winner = calculateWinner(current.squares);
+	  const moves = history.map((step,move)=> {
+		  const desc = move ? 
+		  'Go to mov #' + move :
+		  'Go to Game start';
+		  
+		  return (
+				<li>
+					<button onClick = {()=> this.jumpTo(move)}>{desc}</button>
+				</li>
+		  );
+	  });
 	  let status;
 		if(winner){
 			status = 'Winner ' + winner;
@@ -130,7 +148,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
